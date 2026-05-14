24cbc96059593f280a126c4608b2a5eea54e8843
Tanakrit.k <75963777+tanakrit1@users.noreply.github.com>
2026-05-14 13:41:12 +0700

    change title swagger

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/modules/app.module';
-import { configDotenv } from "dotenv";
+import { configDotenv } from 'dotenv';
 import * as express from 'express';
-import * as path from 'path'
+import * as path from 'path';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import 'reflect-metadata';
 
 configDotenv({ path: '.env' });
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    const config = new DocumentBuilder()
-        .setTitle('Strategy API')
-        .setDescription('The Strategy API description')
-        .setVersion('1.19')
-        .addTag('api')
-        .build();
-    const documentFactory = () => SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('swagger', app, documentFactory);
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('E-Personnel API')
+    .setDescription('The E-Personnel API description')
+    .setVersion('1.19')
+    .addTag('api')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory);
 
-    //   app.useGlobalPipes(new ValidationPipe())
-    app.useGlobalPipes(new ValidationPipe( {
-            transform: true, // ให้ class-transformer ทำงานด้วย
-            whitelist: true, // กรอง property ที่ไม่ได้อยู่ใน DTO ออก
-            forbidNonWhitelisted: true, // ถ้าเจอ property แปลก ให้ throw error
-        } ))
-    app.enableCors()
+  //   app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, // ให้ class-transformer ทำงานด้วย
+      whitelist: true, // กรอง property ที่ไม่ได้อยู่ใน DTO ออก
+      forbidNonWhitelisted: true, // ถ้าเจอ property แปลก ให้ throw error
+    }),
+  );
+  app.enableCors();
 
-    app.use(express.static(path.join(__dirname, '..', 'uploads')));
-    await app.listen(parseInt(process.env.APP_PORT) || 9001);
+  app.use(express.static(path.join(__dirname, '..', 'uploads')));
+  await app.listen(parseInt(process.env.APP_PORT) || 9001);
 }
 bootstrap();
